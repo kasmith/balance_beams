ea8f9f8976dfcbd5f6c4ae1d4f15d5c42e556066
kasmith <k2smith@mit.edu>
2020-04-02 15:53:00 -0400

    Cluster / script indexing

--- Modeling/cluster_crossval_run.py
+++ Modeling/cluster_crossval_run.py
@@ -53,8 +53,9 @@ def do_run(fit, strat, db, dbsize, cores=30):
         else:
             fl.write("#SBATCH -t 2-0\n")
         fl.write("#SBATCH --array=[0-" + str(dbsize-1) + "]\n\n")
+        fl.write("IDX=$(($SLURM_ARRAY_TASK_ID-1))")
         fl.write("python run_crossval_strat.py -d -c run -f " + fit +
-                 " -s " + strat + " -n $SLURM_ARRAY_TASK_ID -i " + ifnm)
+                 " -s " + strat + " -n $IDX -i " + ifnm)
         if strat == "rules" and (fit == "joint_percept" or fit == "individual"):
             fl.write(" --single_strat")
         fl.write(" --hdf " + db)
