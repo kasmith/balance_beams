e6703746954724f5632629486b531253e0f5506c
kasmith <k2smith@mit.edu>
2020-04-02 13:35:18 -0400

    Typo

--- Modeling/cluster_crossval_run.py
+++ Modeling/cluster_crossval_run.py
@@ -43,9 +43,9 @@ def do_run(fit, strat, db, dbsize, cores=30):
         fl.write("#!/usr/bin/env bash\n")
         fl.write("#SBATCH --job-name=BB_CV_" + shorten[fit] + "_" + strat + "\n")
         fl.write("#SBATCH --output=SlurmOut/BBCV_" + shorten[fit] + "_" +
-                 strat + "%A_%a.out\n")
+                 strat + "_%A_%a.out\n")
         fl.write("#SBATCH --output=SlurmOut/BBCV_" + shorten[fit] + "_" +
-                 strat + "%A_%a.err\n")
+                 strat + "_%A_%a.err\n")
         fl.write("#SBATCH --mem=32000\n")
         fl.write("#SBATCH -c " + str(cores) + "\n")
         if fit == "individual" or fit == "joint_percept" or strat == "inc_dist":
